e811bdc611f5fee90e355d4c977faa10a12e71d8
Mira090 <2935shu4182@gmail.com>
2026-08-18 01:51:07 +0900

    融合槍に紅に染まる夕焼けやソリス・デクリなどの効果が適用されない問題を修正

--- MiraItemMod/Items/Machina/Charm_MachinaFlameSpear.cs
+++ MiraItemMod/Items/Machina/Charm_MachinaFlameSpear.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace MiraItemMod.Items.Machina
 {
     public class Charm_MachinaFlameSpear : Charm_Machina
     {
+        private bool recentLuck;
+        public override float RangeBonus => recentLuck ? base.RangeBonus : base.RangeBonus + 0.33f;
         protected override void Awake()
         {
             damageByLevel = new int[10] { 115, 120, 130, 140, 150, 160, 170, 190, 210, 240 };
@@ -24,8 +27,27 @@ namespace MiraItemMod.Items.Machina
         protected override float ModifyDamage(float damage)
         {
             damage += damage * NetworkAvatar.GetCustomStatUnsafe("FLAMESWORDDAMAGE") / 100f;
+            recentLuck = ComboEffect_FlameSword.CheckLuck(NetworkAvatar);
+            if (recentLuck)
+            {
+                int luck = 100 + KeywordDatabase.GetConstValue("flameSwordLuckBonusDamagePercent");
+                damage += damage * luck / 100f;
+            }
+            if (NetworkAvatar.GetCustomStatUnsafe("FLAMESWORDMAGICDAMAGE") > 0)
+            {
+                damage += damage * (NetworkAvatar.GetCustomStat(ECustomStat.MagicDamageBonus) / 100f);
+            }
             return base.ModifyDamage(damage);
         }
         ///パッチはCharm_EmberFlameSword
+
+        protected override void OnCreateAttack(int idx, ProjectileBase projectile)
+        {
+            if (NetworkAvatar == null || projectile == null)
+                return;
+            projectile.additionalCriticalChancePercent += NetworkAvatar.GetCustomStatUnsafe("FLAMESWORDCRITICAL");
+            projectile.additionalCriticalDamageRate += NetworkAvatar.GetCustomStatUnsafe("FLAMESWORDCRITICALDAMAGERATE");
+            projectile.ignoreDefense += NetworkAvatar.GetCustomStatUnsafe("FLAMESWORDIGNOREDEFENSE");
+        }
     }
 }
