4d3e1fe8e6b9fa5c790b6517884b706f98ca5b5b
Mira090 <2935shu4182@gmail.com>
2026-09-24 16:45:41 +0900

    パラス強化アーティファクトに枠線を追加

--- MiraItemMod/Items/Pallas/Charm_PallasEnhancer.cs
+++ MiraItemMod/Items/Pallas/Charm_PallasEnhancer.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using MiraItemMod.Items.Machina;
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 
 namespace MiraItemMod.Items.Pallas
 {
-    public abstract class Charm_PallasEnhancer : Charm_StatusInstance
+    public abstract class Charm_PallasEnhancer : Charm_StatusInstance, IDependencyConditionCharm
     {
 
         protected List<CustomPallasController> Cards = new List<CustomPallasController>();
@@ -21,15 +23,6 @@ namespace MiraItemMod.Items.Pallas
             base.OnDisabledEffect();
             ClearCard();
         }
-        public override void OnCharmEffectRefreshed()
-        {
-            base.OnCharmEffectRefreshed();
-            ClearCard();
-            if (IsEffectEnabled)
-            {
-                SearchCard();
-            }
-        }
 
         private void ClearCard()
         {
@@ -44,6 +37,19 @@ namespace MiraItemMod.Items.Pallas
 
         private void SearchCard()
         {
+            foreach(var charm in GetPallasCards())
+            {
+                if (charm != null && (charm is Charm_PallasCard || charm is Charm_PallasAce) && charm.TryGetComponent<CustomPallasController>(out var pallas))
+                {
+                    SetStats(pallas);
+                    pallas.UpdateStats();
+                    Cards.Add(pallas);
+                }
+            }
+        }
+        protected List<Charm_Basic> GetPallasCards()
+        {
+            var list = new List<Charm_Basic>();
             ItemPosition[] array = GetDirections();
             foreach (ItemPosition itemPosition in array)
             {
@@ -51,16 +57,91 @@ namespace MiraItemMod.Items.Pallas
                 if (newItemOwnInstance != null)
                 {
                     Charm_Basic charm = newItemOwnInstance.Charm;
-                    if (charm != null && (charm is Charm_PallasCard || charm is Charm_PallasAce) && charm.TryGetComponent<CustomPallasController>(out var pallas))
-                    {
-                        SetStats(pallas);
-                        pallas.UpdateStats();
-                        Cards.Add(pallas);
-                    }
+                    if (charm == null)
+                        continue;
+                    list.Add(charm);
                 }
             }
+            return list;
         }
         protected abstract void SetStats(CustomPallasController card);
         protected abstract void ClearStats(CustomPallasController card);
+
+
+        public bool dependency;
+        public List<ItemPosition> positions = new List<ItemPosition>();
+
+        public bool IsDependencyValid(Charm_Basic request)
+        {
+            if (request == null)
+                return false;
+            if (request is Charm_PallasCard || request is Charm_PallasAce)
+            {
+                ItemPosition[] array = GetDirections();
+                foreach (ItemPosition itemPosition in array)
+                {
+                    if (Item.XIdx + itemPosition.x == request.xIdx && Item.YIdx + itemPosition.y == request.yIdx)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+
+        public override void OnCharmEffectRefreshed()
+        {
+            base.OnCharmEffectRefreshed();
+            ClearCard();
+            if (IsEffectEnabled)
+            {
+                SearchCard();
+            }
+
+            if (dependency)
+            {
+                ClearDependency();
+                dependency = false;
+            }
+
+            if (!dependency)
+            {
+                ApplyDependency();
+                dependency = true;
+            }
+        }
+        public override void RefreshCharm()
+        {
+            base.RefreshCharm();
+            if (dependency)
+            {
+                ClearDependency();
+                dependency = false;
+            }
+
+            if (!dependency)
+            {
+                ApplyDependency();
+                dependency = true;
+            }
+        }
+        protected void ClearDependency()
+        {
+            foreach (var pos in positions)
+            {
+                Inventory.RemoveCharmDependency(pos, this);
+            }
+            positions.Clear();
+        }
+        protected void ApplyDependency()
+        {
+            foreach (var charm in GetPallasCards())
+            {
+                if (charm.Item != null && (charm is Charm_PallasCard || charm is Charm_PallasAce))
+                {
+                    Inventory.AddCharmDependency(charm.Item.Position, this);
+                    positions.Add(charm.Item.Position);
+                }
+            }
+        }
     }
 }
